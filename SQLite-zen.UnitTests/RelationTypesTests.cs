0569c2b0faaf3833b0877b57a1df06bd61c10ba9
McFood <matt@litech.com.au>
2020-06-24 17:53:02 +0800

    Added Unit tests project, and one unit test for the new ordering function

---
+++ SQLite-zen.UnitTests/RelationTypesTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SQLite;
+using Xunit;
+
+namespace SQLite_zen.UnitTests {
+    public class RelationTypesTests
+    {
+        private const string DbFilename = "testdb.db3";
+        public RelationTypesTests() {
+            if (File.Exists(DbFilename)) {
+                File.Delete(DbFilename);
+            }
+        }
+
+        [Fact]
+        public void ManyToManyPreservesOrdering()
+        {
+            Parent parent = new Parent() {
+                ID = Guid.NewGuid(),
+                Children = new List<Guid> {
+                    Guid.NewGuid(),
+                    Guid.NewGuid(),
+                    Guid.NewGuid(),
+                },
+            };
+            List<Child> children = new List<Child>();
+            foreach (Guid childId in parent.Children) {
+                children.Add(new Child { ID = childId });
+            }
+            SQLiteConfig config = new SQLiteConfig(CreateFlags.AllImplicit, DbFilename);
+            config
+                .AddTable(typeof(Parent))
+                .AddTable(typeof(Child))
+                .AddTable(typeof(ParentChild))
+                .WireForeignKeys();
+
+            using (SQLiteConnection connection = new SQLiteConnection(config)) {
+                connection.InsertAll(children);
+                connection.Insert(parent);
+                parent.Children.Reverse();
+                connection.Update(parent);
+
+
+            }
+
+            using (SQLiteConnection connection = new SQLiteConnection(config)) {
+                Parent actualParent = connection.Table<Parent>().First();
+                Assert.Equal(3, actualParent.Children.Count);
+                for(int i = 0; i < actualParent.Children.Count; i++) {
+                    Assert.Equal(parent.Children[i], actualParent.Children[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ SQLite-zen.UnitTests/TestModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SQLite_zen.UnitTests {
+    class Parent {
+
+        public Guid ID { get; set; }
+
+        [ManyToMany(
+            typeof(Child),
+            typeof(ParentChild),
+            nameof(ParentChild.Parent),
+            nameof(ParentChild.Child),
+            nameof(ParentChild.Index))]
+        public List<Guid> Children { get; set; }
+    }
+
+    class Child {
+
+        public Guid ID { get; set; }
+    }
+
+    class ParentChild {
+        [ForeignKey(typeof(Parent))]
+        public Guid Parent { get; set; }
+
+        [ForeignKey(typeof(Child))]
+        public Guid Child { get; set; }
+
+        public int Index { get; set; }
+    }
+}
